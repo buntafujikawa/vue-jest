871aa1ba925f4b3b0f02cb6cb3482fa0557e54d0
buntafujikawa <bunta.fujikawa@gmail.com>
2020-03-29 21:26:39 +0900

    add finding elements

---
+++ tests/unit/Parent.spec.js
@@ -0,0 +1,42 @@
+import { mount, shallowMount } from "@vue/test-utils";
+import Parent from "@/components/Parent.vue";
+import Child from "@/components/Child.vue";
+import ParentWithManyChildren from "@/components/ParentWithManyChildren.vue";
+
+describe("Parent", () => {
+  it("does not render a span", () => {
+    const wrapper = shallowMount(Parent);
+    expect(wrapper.find("span").isVisible()).toBe(false);
+  });
+
+  it("renders a span", () => {
+    const wrapper = shallowMount(Parent, {
+      data() {
+        return { showSpan: true };
+      }
+    });
+    expect(wrapper.find("span").isVisible()).toBe(true);
+  });
+
+  it("does not render a Child component", () => {
+    const wrapper = shallowMount(Parent);
+
+    expect(wrapper.find(Child).exists()).toBe(false);
+  });
+
+  it("renders a Child component", () => {
+    const wrapper = shallowMount(Parent, {
+      data() {
+        return { showChild: true };
+      }
+    });
+
+    expect(wrapper.find({ name: "Child" }).exists()).toBe(true);
+  });
+
+  it("renders many children", () => {
+    const wrapper = shallowMount(ParentWithManyChildren)
+
+    expect(wrapper.findAll(Child).length).toBe(3)
+  })
+});
