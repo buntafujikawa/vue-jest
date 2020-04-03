fd75de8b5cbe3c995f90bdd11fad1a1042037747
buntafujikawa <bunta.fujikawa@gmail.com>
2020-04-03 22:57:08 +0900

    add more router test

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -2,7 +2,7 @@ import { mount, createLocalVue } from "@vue/test-utils";
 import App from "@/App.vue";
 import VueRouter from "vue-router";
 import NestedRoute from "@/components/NestedRoute.vue";
-import routes from "@/router/index.js";
+import routes from "@/routes.js"
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
--- tests/unit/NestedRoute.spec.js
+++ tests/unit/NestedRoute.spec.js
@@ -1,18 +1,31 @@
-import { shallowMount } from "@vue/test-utils"
-import NestedRoute from "@/components/NestedRoute.vue"
-import routes from "@/router/index.js"
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import NestedRoute from "@/components/NestedRoute.vue";
+import VueRouter from "vue-router"
+import mockModule from "@/bust-cache.js"
+
+jest.mock("@/bust-cache.js", () => ({ bustCache: jest.fn() }))
+const localVue = createLocalVue()
+localVue.use(VueRouter)
 
 describe("NestedRoute", () => {
   it("renders a username from query string", () => {
-    const username = 'alice'
+    const username = "alice";
     const wrapper = shallowMount(NestedRoute, {
-      mocks:{
+      mocks: {
         $route: {
-          params:{ username }
+          params: { username }
         }
       }
-    })
+    });
 
-    expect(wrapper.find('.username').text()).toBe(username)
+    expect(wrapper.find(".username").text()).toBe(username);
+  });
+
+  it("calls bustCache and next when leaving the route", () => {
+    const next = jest.fn()
+    NestedRoute.beforeRouteLeave(undefined, undefined, next)
+
+    expect(mockModule.bustCache).toHaveBeenCalled()
+    expect(next).toHaveBeenCalled()
   })
-})
+});
